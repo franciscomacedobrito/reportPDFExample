e19aee74dc9f09b5cff79fe29f277d2d97ec4521
brf2av <NTIyOTA1MjE0MjY1Oqs4wEDpJzn5lYIOGcDQoQ3sWK20>
2023-04-18 16:33:16 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isDOMPresent:Boolean = typeof document === "object" && !!document;
   title = 'reportPDFPOC';
+  chartOptions = {
+    "responsive": true,
+    "maintainAspectRatio": false,
+  };
+  chartDatasets = [
+    {
+      "data": [
+        586,
+        61,
+        0,
+        0
+      ],
+      "backgroundColor": [
+        "#ed0007",
+        "#c535bc",
+        "#56b0ff",
+        "#007bc0"
+      ],
+      "hoverBackgroundColor": [
+        "#be0004",
+        "#9e2896",
+        "#0096e8",
+        "#00629a"
+      ],
+      "borderWidth": 0
+    }
+  ];
+
+  ngOnInit(): void {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgChartsModule} from "ng2-charts";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'serverApp'}),
+        NgChartsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
